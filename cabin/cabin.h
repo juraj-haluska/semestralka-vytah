3fd34f958c18aad1b73bbd116eb6c5eaaeb3eb24
spacive <haluskajuraj0@gmail.com>
2017-12-18 02:04:50 +0100

    working on doxygen

--- cabin/cabin.h
+++ cabin/cabin.h
@@ -11,6 +11,7 @@
 
 #include "protocol.h"
 
+//! cabin commands
 #define CABIN_LOCK      0x01
 #define CABIN_UNLOCK    0x00
 #define BREAK_UNLOCK    0x00
@@ -18,13 +19,16 @@
 
 class Cabin {
 private:
-  static const int maxDataLength = 1;
-  uint8_t myAddr;
-  uint8_t breakAddr;
-  uint8_t dataBuffer[maxDataLength];
-  packet_t packet;
-  Protocol *protocol;
-  InterruptIn * freeFall;
+  //! private attributes
+  static const int maxDataLength = 1;       /*!< maximal length of dataBuffer */  
+  uint8_t myAddr;                           /*!< address of cabin lock */  
+  uint8_t breakAddr;                        /*!< address of emergency break */ 
+  uint8_t dataBuffer[maxDataLength];        /*!< data buffer */ 
+  packet_t packet;                          /*!< packet */ 
+  Protocol *protocol;                       /*!< pointer to global protocol */ 
+  InterruptIn * freeFall;                   /*!< pointer to free fall external interrupt */ 
+
+  //! private functions
   void freeFallHandler();
   bool freeFallDetected;
 public:
@@ -40,13 +44,22 @@ public:
 
       freeFallDetected = false;
 
-      // activate external interrupt
+      //! activate external interrupt
       freeFall->fall(callback(this, &Cabin::freeFallHandler));
   };
-  void lock();
+  //! lock the elevator's cabin
+  void lock();    
+  //! unlock the elevator's cabin                     
   void unlock();
+  //! activates emergency break
   void activateBreak();
+  //! deactivates emergency break
   void deactivateBreak();
+  //! Freefall check
+  /**
+   * if free fall occured this function will activate
+   * emergency break
+  */
   void checkFreeFall();
 };
 
